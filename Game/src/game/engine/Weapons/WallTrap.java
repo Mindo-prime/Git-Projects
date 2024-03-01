ca4a52abd3e17cb28b69528febf30fefe939bbf9
alpha-mindo <abdelrahmansalahsalem@gmail.com>
2024-03-01 11:09:44 +0200

    ''

--- Game/src/game/engine/Weapons/WallTrap.java
+++ Game/src/game/engine/Weapons/WallTrap.java
@@ -1,7 +1,7 @@
 package game.engine.Weapons;
 
 public class WallTrap extends Weapons {
-
+    public static final int WEAPON_CODE = 4;
     public WallTrap(int BaseDamage) {
         super(BaseDamage);
     }
